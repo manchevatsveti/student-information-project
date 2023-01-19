f1106fa0d779733b61cd81589b18d40cef7766d7
Tsvetelina Mancheva <mancheva_tsvetelina@DESKTOP-A9E3DQE>
2023-01-19 13:22:26 +0200

    Sorting and rearranging the code based on functionality. Added validation for the sorting criteria.

--- student_information_project/main.cpp
+++ student_information_project/main.cpp
@@ -1,4 +1,3 @@
-
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -7,24 +6,32 @@
 using namespace std;
 
 void printMenu();
-bool userInputValidation(string input);
+
+//main menu actions
 void addingStudent(string fileName, string fn, string name, string countSubjects);
 void deletingStudent(string facultyNumberToDelete, string fileName);
 void sortStudents(int sortBy, int sortOrder, string fileName);
+void printFile(string fileName);
+void printMultipleFiles(int countGroups);
+
+//utility functions
 void writingInVectorBasedOnSortCriteria(string fileName, int sortBy, vector<string>& students);
 void rewriteVector(vector<string>& students);
 void writingInFileInAscendingOrder(vector<string>& students, string fileName);
 void writingInFileInDescendingOrder(vector<string>& students, string fileName);
-void printFile(string fileName); 
-void printMultipleFiles(int countGroups);
 string fileNameGenerator(string groupNum);
-bool isGroupNumberValid(string group);
+void combineMultipleFilesInOne(int countGroups);
+
+//validation functions
+bool userInputValidation(string input);
 bool isFnUnique(string fileName, string fn);
+bool isGroupNumberValid(string group);
+bool isSortCriteriaValid(int sortCriteria);
 
 int main() {
 
-	string sortBy, sortOrder,input,fn,name, countSubjects, countGroupsToPrint, student,group,fileName;
-	bool isInputValid = false, validCountSubjects=false, validCountGroups = false, validGroupNumber=false, validFn=false;
+	string sortBy, sortOrder, input, fn, name, countSubjects, countGroupsToPrint, student, group, fileName;
+	bool isInputValid = false, validCountSubjects = false, validCountGroups = false, validGroupNumber = false, validFn = false, validSortCriteria = false;
 
 	cout << "Group: " << endl;
 	while (!validGroupNumber) {
@@ -52,7 +59,7 @@ int main() {
 				cout << "Faculty number:" << endl;
 				while (!validFn) {
 					getline(cin, fn);
-					validFn = isFnUnique(fileName,fn);
+					validFn = isFnUnique(fileName, fn);
 
 					if (!validFn) {
 						cout << "This faculty number already exists. Please try again." << endl;
@@ -63,7 +70,7 @@ int main() {
 				cout << "Name of student:" << endl;
 				getline(cin, name);
 				cout << "Number of subjects studied: " << endl;
-				while (validCountSubjects != true ) {
+				while (!validCountSubjects) {
 					getline(cin, countSubjects);
 					if (stoi(countSubjects) < 1 || stoi(countSubjects) > 10) {
 						cout << "Count should be between 1 and 10. Please try again!" << endl;
@@ -81,9 +88,27 @@ int main() {
 				break;
 			case 3:
 				cout << "Sort by: " << endl << "1. Faculty number" << endl << "2. Average score" << endl;
-				getline(cin, sortBy);
+
+				while (validSortCriteria != true) {
+					getline(cin, sortBy);
+					validSortCriteria = isSortCriteriaValid(stoi(sortBy));
+					if (!validSortCriteria) {
+						cout << "Choose between opion 1 and 2. Please try again!" << endl;
+					}
+				}
+				validSortCriteria = false;
+
 				cout << endl << "1. Ascending order" << endl << "2. Descending order" << endl;
-				getline(cin, sortOrder);
+
+				while (validSortCriteria != true) {
+					getline(cin, sortOrder);
+					validSortCriteria = isSortCriteriaValid(stoi(sortOrder));
+					if (!validSortCriteria) {
+						cout << "Choose between opion 1 and 2. Please try again!" << endl;
+					}
+				}
+				validSortCriteria = false;
+
 				sortStudents(stoi(sortBy), stoi(sortOrder), fileName);
 				break;
 			case 4:
@@ -109,12 +134,62 @@ int main() {
 
 			printMenu();
 		}
-	} 
+	}
 }
-bool isFnUnique(string fileName, string fn) {
+
+void printMenu() {
+	cout << "Choose your option: " << endl;
+	cout << "1. Add a student" << endl;
+	cout << "2. Delete a student" << endl;
+	cout << "3. Sort the students " << endl;
+	cout << "4. Print group members " << endl;
+	cout << "5. Print group members from multiple groups " << endl;
+	cout << "6. Exit " << endl;
+
+}
+
+//main menu actions
+void addingStudent(string fileName, string fn, string name, string countSubjects) {
+	string tempGrade;
 	fstream File;
-	string buffer,facultyNumber;
-	bool isUnique = true;
+	double sumGrades = 0, averageScore = 0;
+
+	string* subjects = new string[stoi(countSubjects)];//creating an array with all the subjects
+	int* grades = new int[stoi(countSubjects)];//creating an array with all the grades
+
+	for (size_t i = 0; i < stoi(countSubjects); i++) {
+		cout << "Subject " << i + 1 << ":" << endl;
+		getline(cin, subjects[i]);
+
+		cout << "Grade:" << endl;
+		getline(cin, tempGrade);
+
+		grades[i] = stoi(tempGrade);
+		sumGrades += grades[i];//sum all grades
+	}
+	averageScore = sumGrades / stoi(countSubjects);//average score of all grades
+
+	File.open(fileName, std::fstream::out | std::fstream::app);//writing in file
+	File << fn << ":" << name;
+
+	for (size_t i = 0; i < stoi(countSubjects); i++) {
+		File << ":" << subjects[i] << "-" << grades[i];
+	}
+	File << "; average score /" << averageScore;
+	File << endl;
+
+	delete[] subjects;
+	delete[] grades;
+
+	File.close();
+}
+
+void deletingStudent(string facultyNumberToDelete, string fileName) {
+	fstream File;
+	string buffer, facultyNumber;
+	bool isStudentDeleted = false;
+
+	vector<string> students;
 
 	File.open(fileName, std::fstream::in);
 
@@ -122,29 +197,52 @@ bool isFnUnique(string fileName, string fn) {
 		while (getline(File, buffer) && buffer.size() > 0) {
 
 			int i = 0;
-			while (i < buffer.size() && buffer[i] != ':') {
+			while (i < buffer.size() && buffer[i] != ':') {//looping through the array until ':', meaning that is the end of the faculty number, based on how we store the student's information
+
 				facultyNumber += buffer[i++];
+
 			}
-			if (fn == facultyNumber) {
-				isUnique = false;
-				break;
+			if (facultyNumber != facultyNumberToDelete) {//if the faculty number is not the one we want to delete, then we push this line from the file into the array
+				students.push_back(buffer);
 			}
-			facultyNumber.clear();
+			else if (facultyNumber == facultyNumberToDelete) {
+				isStudentDeleted = true;
+				cout << "The student was successfully deleted!" << endl;
+			}
+			facultyNumber.clear();//clearing the faculty number for the next student
 		}
+		if (!isStudentDeleted) {
+			cout << "There is not a student with this faculty number in this group." << endl;
+		}
+
+		File.close();
+
+		writingInFileInAscendingOrder(students, fileName);
 	}
-	File.close();
-	return isUnique;
 }
-bool isGroupNumberValid(string group) {
-	bool isValid = true;
-	if (stoi(group) < 1 || stoi(group) > 8) {
-		isValid = false;;
+
+void sortStudents(int sortBy, int sortOrder, string fileName) {
+	vector<string> students;
+
+	writingInVectorBasedOnSortCriteria(fileName, sortBy, students);
+
+	sort(students.begin(), students.end());
+
+	if (sortBy == 2) {//if we sorted the vector based on average score, rewriting the elements, so when we write back in the file, the inforamtion is in its noramal order
+		rewriteVector(students);
 	}
-	return isValid;
+
+	if (sortOrder == 1) {//ascending order
+		writingInFileInAscendingOrder(students, fileName);
+	}
+	else if (sortOrder == 2) {//descending order
+		writingInFileInDescendingOrder(students, fileName);
+	}
 }
+
 void printFile(string fileName) {
 	fstream File;
-	string buffer, name,facultyNumber, averageScore;
+	string buffer, name, facultyNumber, averageScore;
 
 	File.open(fileName, std::fstream::in);
 
@@ -181,123 +279,21 @@ void printFile(string fileName) {
 	File.close();
 }
 
-void printMenu() {
-	cout << "Choose your option: " << endl;
-	cout << "1. Add a student" << endl;
-	cout << "2. Delete a student" << endl;
-	cout << "3. Sort the students " << endl;
-	cout << "4. Print group members " << endl;
-	cout << "5. Print group members from multiple groups " << endl;
-	cout << "6. Exit " << endl;
+void printMultipleFiles(int countGroups) {
+	string sortBy, sortOrder;
 
-}
-bool isNumber(const string& s)
-{
-	for (char const& ch : s) {
-		if (isdigit(ch) == 0)
-			return false;
-	}
-	return true;
-}
-bool userInputValidation(string input) {
-		bool isValidInput = true;
+	combineMultipleFilesInOne(countGroups);
 
-		if (isNumber(input)) {
-			if (stoi(input) < 1 || stoi(input) > 6) {
-				isValidInput = false;
-			}
-		}
-		return isValidInput;
-}
-void addingStudent(string fileName, string fn, string name, string countSubjects) {
-	string tempGrade;
-	fstream File;
-	double sumGrades = 0,averageScore=0;
-	
-	string* subjects = new string[stoi(countSubjects)];//creating an array with all the subjects
-	int* grades = new int[stoi(countSubjects)];//creating an array with all the grades
+	cout << "Sort by: " << endl << "1. Faculty number" << endl << "2. Average score" << endl;
+	getline(cin, sortBy);
+	cout << endl << "1. Ascending order" << endl << "2. Descending order" << endl;
+	getline(cin, sortOrder);
+	sortStudents(stoi(sortBy), stoi(sortOrder), "bundle.txt");
 
-	for (size_t i = 0; i < stoi(countSubjects); i++) {
-		cout << "Subject " << i+1 << ":" << endl;
-		getline(cin, subjects[i]);
-
-		cout << "Grade:" << endl;
-		getline(cin, tempGrade);
-
-		grades[i] = stoi(tempGrade);
-		sumGrades += grades[i];//sum all grades
-	}
-	averageScore = sumGrades / stoi(countSubjects);//average score of all grades
-
-	File.open(fileName, std::fstream::out | std::fstream::app);//writing in file
-	File << fn<<":"<<name;
-
-	for (size_t i = 0; i < stoi(countSubjects); i++) {
-		File << ":" << subjects[i] << "-" << grades[i];
-	}
-	File << "; average score /" << averageScore;
-	File << endl;
-
-	delete[] subjects;
-	delete[] grades;
-
-	File.close();
+	printFile("bundle.txt");
 }
 
-void deletingStudent(string facultyNumberToDelete, string fileName) {
-	fstream File;
-	string buffer,facultyNumber;
-	bool isStudentDeleted = false;
-
-	vector<string> students;
-
-	File.open(fileName, std::fstream::in);
-
-	if (File.is_open()) {
-		while (getline(File, buffer) && buffer.size() > 0) {
-
-			int i = 0;
-			while (i < buffer.size() && buffer[i] != ':') {//looping through the array until ':', meaning that is the end of the faculty number, based on how we store the student's information
-
-				facultyNumber += buffer[i++];
-
-			}
-			if (facultyNumber != facultyNumberToDelete) {//if the faculty number is not the one we want to delete, then we push this line from the file into the array
-				students.push_back(buffer);
-			}
-			else if (facultyNumber == facultyNumberToDelete) {
-				isStudentDeleted = true;
-				cout << "The student was successfully deleted!" << endl;
-			}
-			facultyNumber.clear();//clearing the faculty number for the next student
-		}
-		if (!isStudentDeleted) {
-			cout << "There is not a student with this faculty number in this group." << endl;
-		}
-
-		File.close();
-
-		writingInFileInAscendingOrder(students, fileName);
-	}
-}
-void sortStudents(int sortBy, int sortOrder, string fileName) {
-	vector<string> students;
-
-	writingInVectorBasedOnSortCriteria(fileName, sortBy, students);
-
-	sort(students.begin(), students.end());
-
-	if (sortBy == 2) {//if we sorted the vector based on average score, rewriting the elements, so when we write back in the file, the inforamtion is in its noramal order
-		rewriteVector(students);
-	}
-
-	if (sortOrder == 1) {//ascending order
-		writingInFileInAscendingOrder(students, fileName);
-	}
-	else if (sortOrder == 2) {//descending order
-		writingInFileInDescendingOrder(students, fileName);
-	}
-}
+//utility functions
 void writingInVectorBasedOnSortCriteria(string fileName, int sortBy, vector<string>& students) {
 	fstream File;
 	string buffer, averageScore, studentInfo;
@@ -333,7 +329,7 @@ void writingInVectorBasedOnSortCriteria(string fileName, int sortBy, vector<stri
 	File.close();
 }
 
-void rewriteVector(vector<string>& students) {
+void rewriteVector(vector<string>& students) {//rewriting the vector in its original format(average score is in the end of every line)
 	string averageScore, studentInfo;
 
 	for (int i = 0; i < students.size(); ++i)
@@ -353,6 +349,7 @@ void rewriteVector(vector<string>& students) {
 		averageScore.clear();
 	}
 }
+
 void writingInFileInAscendingOrder(vector<string>& students, string fileName) {
 	fstream File;
 
@@ -365,6 +362,7 @@ void writingInFileInAscendingOrder(vector<string>& students, string fileName) {
 
 	File.close();
 }
+
 void writingInFileInDescendingOrder(vector<string>& students, string fileName) {
 	fstream File;
 
@@ -377,6 +375,7 @@ void writingInFileInDescendingOrder(vector<string>& students, string fileName) {
 
 	File.close();
 }
+
 string fileNameGenerator(string groupNum) {
 	string fileName = "group_";
 	fileName += groupNum;
@@ -387,7 +386,7 @@ string fileNameGenerator(string groupNum) {
 
 void combineMultipleFilesInOne(int countGroups) {
 	fstream File;
-	string fileBundle = "bundle.txt",buffer;
+	string fileBundle = "bundle.txt", buffer;
 	bool validGroupNumber = false;
 
 	string* groups = new string[countGroups];//creating an array with all the grades
@@ -421,16 +420,57 @@ void combineMultipleFilesInOne(int countGroups) {
 		File.close();
 	}
 }
-void printMultipleFiles(int countGroups){
-	string sortBy, sortOrder;
 
-	combineMultipleFilesInOne(countGroups);
+//validation functions
+bool userInputValidation(string input) {
+	bool isValidInput = true;
 
-	cout << "Sort by: " << endl << "1. Faculty number" << endl << "2. Average score" << endl;
-	getline(cin, sortBy);
-	cout << endl << "1. Ascending order" << endl << "2. Descending order" << endl;
-	getline(cin, sortOrder);
-	sortStudents(stoi(sortBy), stoi(sortOrder), "bundle.txt");
-
-	printFile("bundle.txt");
+	if (stoi(input) < 1 || stoi(input) > 6) {
+		isValidInput = false;
+	}
+	return isValidInput;
 }
+
+bool isFnUnique(string fileName, string fn) {
+	fstream File;
+	string buffer, facultyNumber;
+	bool isUnique = true;
+
+	File.open(fileName, std::fstream::in);
+
+	if (File.is_open()) {
+		while (getline(File, buffer) && buffer.size() > 0) {
+
+			int i = 0;
+			while (i < buffer.size() && buffer[i] != ':') {
+				facultyNumber += buffer[i++];
+			}
+			if (fn == facultyNumber) {
+				isUnique = false;
+				break;
+			}
+			facultyNumber.clear();
+		}
+	}
+	File.close();
+	return isUnique;
+}
+
+bool isGroupNumberValid(string group) {
+	bool isValid = true;
+	if (stoi(group) < 1 || stoi(group) > 8) {
+		isValid = false;;
+	}
+	return isValid;
+}
+
+bool isSortCriteriaValid(int sortCriteria) {
+	bool isValid = false;
+
+	if (sortCriteria == 1 || sortCriteria == 2) {
+		isValid = true;
+	}
+
+	return isValid;
+}
+
